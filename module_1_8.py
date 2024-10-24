c660498129a5fa706d872f0ea366a83d8e86be36
seelk <sedova08111984@gmail.com>
2024-10-24 21:26:25 +0300

    Add files via upload

---
+++ module_1_8.py
@@ -0,0 +1,12 @@
+my_dict = {'Мари' : 2006, 'Настя' : 2013, 'Дима' : 1999}
+print(my_dict)
+my_dict.update({'Никита' : 2000, 'Катя' : 2005})
+print(my_dict)
+my_dict.pop('Настя')
+print(my_dict)
+
+my_set = {11,44,19,100,11,100,'код','сервер','код', True, True, False, True}
+print(my_set)
+my_set.add('сыр')
+my_set.discard(44)
+print(my_set)
