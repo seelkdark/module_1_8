aa82b3d4e6bf3aed68d3460f99fc2943010921bb
seelk <sedova08111984@gmail.com>
2024-11-10 23:37:08 +0300

    Update module_1_8.py

--- module_1_8.py
+++ module_1_8.py
@@ -1,12 +1,16 @@
-my_dict = {'Мари' : 2006, 'Настя' : 2013, 'Дима' : 1999}
-print(my_dict)
-my_dict.update({'Никита' : 2000, 'Катя' : 2005})
-print(my_dict)
-my_dict.pop('Настя')
-print(my_dict)
-
 my_set = {11,44,19,100,11,100,'код','сервер','код', True, True, False, True}
 print(my_set)
 my_set.add('сыр')
 my_set.discard(44)
 print(my_set)
+
+my_dict = {'Мари' : 2006, 'Настя' : 2013, 'Дима' : 1999, 'Никита': 1984}
+print('Словарь:', my_dict)
+print('Год рождения Мари:', my_dict['Мари'])
+print('Год рождения Никита:', my_dict.get('Никита', 'нет такого ключа'))
+my_dict.update({'Слава' : 2001, 'Катя' : 2009})
+removed_year = my_dict.pop('Слава')
+print('Значение удалённого элемента \'Слава\':', removed_year)
+print('Изменённый словарь:', my_dict)
+
+print()
